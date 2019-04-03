e168bcb5ddc920bd0fe28b4ae1a6c41d2bf03270
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-03 12:05:07 -0400

    Switched name of main script and added running main to ci

--- .ci/construct_test_command.sh
+++ .ci/construct_test_command.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 token=d9212b19-6bbd-4466-8f45-18414480f879
+cd_repo="cd /root/mantid_total_scattering"
 ci_env="bash <(curl -s https://codecov.io/env)"
+run_pytest="pytest"
+run_main="mantidpython --classic ./bin/main.py --help"
+run_coverage="coverage run -m pytest ."
 ci_post_cov="bash <(curl -s https://codecov.io/bash) -t $token"
-test_cmd="$ci_env && pytest /root/mantid_total_scattering/ && coverage run -m pytest /root/mantid_total_scattering/  && $ci_post_cov"
+
+test_cmd="$cd_repo && $ci_env && $run_pytest && $run_main && $run_coverage && $ci_post_cov"
 
 echo $test_cmd
 
