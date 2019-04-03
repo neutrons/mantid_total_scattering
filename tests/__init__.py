e77e3cd5d329b7b5d251e45f7d8f4b40da7171c5
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-03 12:59:40 -0400

    Moved test_data -> tests/data

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ from total_scattering.utils import ROOT_DIR
 # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
 IN_TRAVIS = os.getenv('TRAVIS', False)
 EXAMPLE_DIR = os.path.join(ROOT_DIR, 'examples')
-TEST_DATA_DIR = os.path.join(ROOT_DIR, 'test_data')
+TEST_DATA_DIR = os.path.join(ROOT_DIR, 'tests', 'data')
