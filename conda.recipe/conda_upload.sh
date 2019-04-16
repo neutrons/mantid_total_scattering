1cfc2e685dd99334eba8c5d06a7d5af0247cb544
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-16 15:34:07 -0400

    Captured build sha from package for conda upload

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -2,10 +2,11 @@
 PKG_NAME=mantid_total_scattering
 USER=marshallmcdonnell
 
-OS=$TRAVIS_OS_NAME-64
+OS=${TRAVIS_OS_NAME}-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
+export BUILD=$(ls ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}* | sed -n 's/.*${PKG_NAME}-\(.*\)-\(.*\)\.tar.bz2/\1-\2/p'
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${USER} -l nightly ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}-${BUILD}.tar.bz2 --force
 
