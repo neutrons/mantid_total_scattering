d5a05626821eadc3aa7502f4f4748423edd92123
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-16 15:57:57 -0400

    Debug for upload  to conda

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -7,6 +7,7 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 conda build .
-export BUILD=$(ls ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}* | sed -n 's/.*${PKG_NAME}-\(.*\)-\(.*\)\.tar.bz2/\1-\2/p'
+export BUILD=$(ls ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}* | sed -n 's/.*${PKG_NAME}-\(.*\)-\(.*\)\.tar.bz2/\1-\2/p')
+echo "Uploading mantid-total-scattering-${BUILD}.tar.bz2 artifact..."
 anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${USER} -l nightly ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}-${BUILD}.tar.bz2 --force
 
