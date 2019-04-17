abcde3948986b41c43445566ba1d6a1dbb9912c8
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-17 15:06:10 -0400

    Remove nightly tag on conda upload for testing

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -9,5 +9,5 @@ export CONDA_BLD_PATH=~/conda-bld
 conda build .
 export BUILD=$(ls ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}* | sed -n "s/.*${PKG_NAME}-\(.*\)-\(.*\)\.tar.bz2/\1-\2/p")
 echo "Uploading ${PKG_NAME}-${BUILD}.tar.bz2 artifact..."
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${USER} -l nightly ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}-${BUILD}.tar.bz2 --force
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${USER} ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}-${BUILD}.tar.bz2 --force
 
