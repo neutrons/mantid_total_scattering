3e220abd5cea46286c10a85c6ce80e9b62b38b67
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-07-15 16:47:37 -0400

    Fix coverage reporting

--- .ci/construct_test_command.sh
+++ .ci/construct_test_command.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 token=d9212b19-6bbd-4466-8f45-18414480f879
+coverage_bin="which $(coverage)"
 cd_repo="cd /root/mantid_total_scattering"
 ci_env="bash <(curl -s https://codecov.io/env)"
-run_pytest="mantidpython -m pytest"
-run_main="mantidpython --classic ./bin/mantidtotalscattering --help"
-run_coverage="coverage run -m pytest ."
+run_main="mantidpython --classic ./total_scattering/cli.py --help"
+run_pytest_coverage="mantidpython --classic ${coverage_bin} run -m pytest ."
 ci_post_cov="bash <(curl -s https://codecov.io/bash) -t $token"
 
-test_cmd="$cd_repo && $ci_env && $run_pytest && $run_main && $run_coverage && $ci_post_cov"
+test_cmd="$cd_repo && $ci_env && $run_main && $run_pytest_coverage && $ci_post_cov"
 
 echo $test_cmd
 
