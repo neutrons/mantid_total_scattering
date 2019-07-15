bbdab171675a312f9e7e06c46af1672c32f54e59
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-07-15 08:56:35 -0400

    Add ROOT_DIR in tests/__init__.py to fix import error from total_scattering.utils running test suite for Python 2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 import os
-from total_scattering.utils import ROOT_DIR
+from os.path import abspath, dirname, join
 
 # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
 IN_TRAVIS = os.getenv('TRAVIS', False)
+ROOT_DIR = abspath(join(dirname(abspath(__file__)), '..'))
 EXAMPLE_DIR = os.path.join(ROOT_DIR, 'examples')
 TEST_DATA_DIR = os.path.join(ROOT_DIR, 'tests', 'data')
