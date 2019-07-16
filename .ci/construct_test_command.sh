26ef061127e22c4c162275ed8c21efa82fda8ba4
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-07-16 10:43:58 -0400

    Fix for still broken coverage report

--- .ci/construct_test_command.sh
+++ .ci/construct_test_command.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 token=d9212b19-6bbd-4466-8f45-18414480f879
-coverage_bin="which $(coverage)"
+coverage_bin="/usr/local/bin/coverage"
 cd_repo="cd /root/mantid_total_scattering"
 ci_env="bash <(curl -s https://codecov.io/env)"
 run_main="mantidpython --classic ./total_scattering/cli.py --help"
