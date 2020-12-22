9434d3556050ef1e560d1f256c75d234ee0cd90c
Jenna Delozier <jennakate998@gmail.com>
2020-12-22 17:37:10 -0500

    Tests for multiple absorption correction options

--- tests/file_handling/test_load.py
+++ tests/file_handling/test_load.py
@@ -10,7 +10,7 @@ from mantid.simpleapi import mtd
 class TestLoad(unittest.TestCase):
 
     def setUp(self):
-        self.align_and_focus_args = {
+        self.polaris_align_and_focus_args = {
             'CalFilename': os.path.join(EXAMPLE_DIR, 'isis', 'polaris_grouping.cal'),
             'ResampleX': -6000,
             'DSpacing': False,
@@ -18,12 +18,24 @@ class TestLoad(unittest.TestCase):
             'MaxChunkSize': 8,
             'ReductionProperties': '__powderreduction'
         }
+
+        self.nomad_align_and_focus_args = {
+            'TMin': 300.0,
+            'TMax': 16667.0,
+            'CalFilename': os.path.join(EXAMPLE_DIR, 'sns', 'nomad_cal.h5'),
+            'ResampleX': -6000,
+            'DSpacing': False,
+            'PreserveEvents': False,
+            'MaxChunkSize': 8,
+            'ReductionProperties': '__powderreduction'}
+
         # Highly cropped version of the workspace to improve run time
-        self.sample_file_path = os.path.join(TEST_DATA_DIR, 'POLARIS00097947-min.nxs')
+        self.si_polaris_file_path = os.path.join(TEST_DATA_DIR, 'POLARIS00097947-min.nxs')
+        self.lab6_nomad_file_path = os.path.join(TEST_DATA_DIR, 'NOM_144992.nxs')
 
     def test_basic_load(self):
         ws_name = 'test-sample'
-        actual = load(ws_name, self.sample_file_path, **self.align_and_focus_args)
+        actual = load(ws_name, self.si_polaris_file_path , **self.polaris_align_and_focus_args)
         actual = mtd[actual]
         self.assertEqual(actual.name(), ws_name)
         self.assertEqual(actual.getNumberHistograms(), 5)
@@ -39,11 +51,11 @@ class TestLoad(unittest.TestCase):
         formula = 'Si'
         mass_density = 2.328
         actual = load(ws_name=ws_name,
-                      input_files=self.sample_file_path,
+                      input_files=self.si_polaris_file_path ,
                       geometry=geometry,
                       chemical_formula=formula,
                       mass_density=mass_density,
-                      **self.align_and_focus_args)
+                      **self.polaris_align_and_focus_args)
         actual = mtd[actual]
         self.assertEqual(actual.sample().getMaterial().name(), 'Si')
         mtd.clear()
@@ -57,23 +69,88 @@ class TestLoad(unittest.TestCase):
         formula = 'Si'
         mass_density = 2.328
 
-        a_sample, a_container = create_absorption_wksp(self.sample_file_path, "SampleOnly",
+        a_sample, a_container = create_absorption_wksp(self.si_polaris_file_path , "SampleOnly",
                                                        geometry=geometry,
                                                        material={"ChemicalFormula": formula,
                                                                  "SampleMassDensity": mass_density})
         self.assertIsNotNone(a_sample)
 
         actual = load(ws_name=ws_name,
-                      input_files=self.sample_file_path,
+                      input_files=self.si_polaris_file_path ,
                       geometry=geometry,
                       chemical_formula=formula,
                       mass_density=mass_density,
                       absorption_wksp=a_sample,
-                      **self.align_and_focus_args)
+                      **self.polaris_align_and_focus_args)
         actual = mtd[actual]
         self.assertEqual(actual.sample().getMaterial().name(), 'Si')
         mtd.clear()
 
+    def test_load_type(self):
+        ws_name = 'test-sample'
+        geometry = {'Shape': 'Cylinder',
+                    'Radius': 0.3,
+                    'Height': 1.8}
+        formula = 'La1 B6'
+        mass_density = 4.72
+        environment = {'Name': 'InAir', 'Container': "PAC06"}
+
+        # Test SampleOnly
+        a_sample, a_container = create_absorption_wksp(self.lab6_nomad_file_path, "SampleOnly",
+                                                       geometry=geometry,
+                                                       material={"ChemicalFormula": formula,
+                                                                 "SampleMassDensity": mass_density},
+                                                       environment=environment)
+        self.assertIsNotNone(a_sample)
+
+        actual = load(ws_name=ws_name,
+                      input_files=self.lab6_nomad_file_path,
+                      geometry=geometry,
+                      chemical_formula=formula,
+                      mass_density=mass_density,
+                      absorption_wksp=a_sample,
+                      **self.nomad_align_and_focus_args)
+        actual = mtd[actual]
+        self.assertEqual(actual.sample().getMaterial().name(), 'La1 B6')
+        mtd.clear()
+        
+        # Test SampleAndContainer
+        a_sample, a_container = create_absorption_wksp(self.lab6_nomad_file_path, "SampleAndContainer",
+                                                       geometry=geometry,
+                                                       material={"ChemicalFormula": formula,
+                                                                 "SampleMassDensity": mass_density},
+                                                       environment=environment)
+        self.assertIsNotNone(a_sample)
+
+        actual = load(ws_name=ws_name,
+                      input_files=self.lab6_nomad_file_path,
+                      geometry=geometry,
+                      chemical_formula=formula,
+                      mass_density=mass_density,
+                      absorption_wksp=a_sample,
+                      **self.nomad_align_and_focus_args)
+        actual = mtd[actual]
+        self.assertEqual(actual.sample().getMaterial().name(), 'La1 B6')
+        mtd.clear()
+
+        # Test FullPaalmanPings
+        a_sample, a_container = create_absorption_wksp(self.lab6_nomad_file_path, "FullPaalmanPings",
+                                                       geometry=geometry,
+                                                       material={"ChemicalFormula": formula,
+                                                                 "SampleMassDensity": mass_density},
+                                                       environment=environment)
+        self.assertIsNotNone(a_sample)
+
+        actual = load(ws_name=ws_name,
+                      input_files=self.lab6_nomad_file_path,
+                      geometry=geometry,
+                      chemical_formula=formula,
+                      mass_density=mass_density,
+                      absorption_wksp=a_sample,
+                      **self.nomad_align_and_focus_args)
+        actual = mtd[actual]
+        self.assertEqual(actual.sample().getMaterial().name(), 'La1 B6')
+        mtd.clear()
 
 if __name__ == '__main__':
     unittest.main()  # pragma: no cover
