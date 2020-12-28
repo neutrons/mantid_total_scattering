9017ca6ccf473deb1275f7330cf8f0e711b49486
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-12-28 11:57:20 -0500

    Refactor setup.py and add requirements files

--- setup.py
+++ setup.py
@@ -1,27 +1,62 @@
+import os
 from setuptools import setup, find_packages
-import versioneer
+import versioneer  # https://github.com/warner/python-versioneer
 
+# Constants
+THIS_DIR = os.path.dirname(__file__)
+
+# Package description
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='mantid_total_scattering',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      author='Marshall, Elliot, Pete',
-      author_email='mcondonnellmd@ornl.gov',
-      url='https://github.com/marshallmcdonnell/mantid_total_scattering',
-      description='Mantid Total Scattering Reduction',
-      long_description_content_type="text/markdown",
-      license='GPL License (version 3)',
-      entry_points={
-        'console_scripts': [
-            "mantidtotalscattering = total_scattering.cli:main"
-        ]
-      },
-      install_requires=['six>=1.9'],
-      packages=find_packages(),
-      # package_data={'': ['*.ui', '*.png', '*.qrc', '*.json']},
-      include_package_data=True,
-      test_suite='tests',
-      tests_require=['pytest']
-      )
+# Package requirements helper
+def read_requirements_from_file(filepath):
+    '''Read a list of requirements from the given file and split into a
+    list of strings. It is assumed that the file is a flat
+    list with one requirement per line.
+    :param filepath: Path to the file to read
+    :return: A list of strings containing the requirements
+    '''
+    with open(filepath, 'rU') as req_file:
+        return req_file.readlines()
+
+setup_args = dict(
+    install_requires=read_requirements_from_file(
+        os.path.join(
+            THIS_DIR,
+            'requirements.txt')),
+    tests_require=read_requirements_from_file(
+        os.path.join(
+            THIS_DIR,
+            'requirements-dev.txt')))
+
+# Author list
+authors = [
+    'Marshall McDonnell',
+    'Peter Peterson',
+    'Elliot Oram',
+]
+
+# Main setup
+setup(
+    name='mantid_total_scattering',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author=','.join(authors)
+    author_email='mcondonnellmd@ornl.gov',
+    url='https://github.com/neutrons/mantid_total_scattering',
+    description='Mantid Total Scattering Reduction',
+    long_description_content_type="text/markdown",
+    license='GPL License (version 3)',
+    entry_points={
+      'console_scripts': [
+          "mantidtotalscattering = total_scattering.cli:main"
+      ]
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=[],
+    install_requires=setup_args['install_requires'],
+    tests_require=setup_args['install_requires'] + setup_args['tests_require'],
+    test_suite='tests',
+)
