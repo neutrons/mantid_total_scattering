47b2fe014d6c98cf6a2ec9e264b47aed05353609
Jose Borreguero <borreguero@gmail.com>
2021-07-28 06:53:51 -0400

    Utility to compare two dictionaries of any values
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

---
+++ tests/utils.py
@@ -0,0 +1,28 @@
+r"""
+
+"""
+
+# standard imports
+from typing import Any, Dict
+import unittest
+
+def compare(expected: Dict[str, Any],
+            actual: Dict[str, Any]) -> None:
+    r"""Assert values of two dictionaries
+
+    :param expected: dictionary containing the correct values
+    :param actual: dictionary containing the values to be compared against the correct ones
+    """
+    assertor = unittest.TestCase()
+    comparators = {
+        str: assertor.assertEqual,
+        int: assertor.assertEqual,
+        float: assertor.assertAlmostEqual
+    }
+    for key, value in expected.items():
+        if isinstance(value, dict) and 'Kwargs' in value:
+            comparator = comparators[type(value['Value'])]
+            comparator(value['Value'], actual[key], **value['Kwargs'])
+        else:
+            comparator = comparators[type(value)]
+            comparator(value, actual[key])
