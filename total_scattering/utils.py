e4dc43237e68b9adf2949fd680299783c6ae2eb6
Jose Borreguero <borreguero@gmail.com>
2021-08-24 07:00:27 -0400

    random workspace name
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- total_scattering/utils.py
+++ total_scattering/utils.py
@@ -1,3 +1,17 @@
 from os.path import abspath, dirname, join
 
 ROOT_DIR = abspath(join(dirname(abspath(__file__)), '..'))
+
+def random_name(n: int = 12, prefix: str = '_', suffix: str = '') -> str:
+    r"""
+    Random word intended as the name of a temporary workspace
+
+    Parameters
+    ----------
+    n: length of the word, not counting the prefix and suffix
+    prefix: prepend this string to the random word
+    suffix: append this string to the random word
+    """
+    word = ''.join([random.choice(string.ascii_letters) for _ in range(n)])
+    return prefix + word + suffix
+
