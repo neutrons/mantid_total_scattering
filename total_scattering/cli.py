143a91fccbef2cd00b133acd493f9ca588042fb7
Chen Zhang <zhangc@ornl.gov>
2021-10-01 11:34:01 -0400

    format fixing with black

--- total_scattering/cli.py
+++ total_scattering/cli.py
@@ -1,22 +1,25 @@
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 import json
 from total_scattering.reduction import TotalScatteringReduction
 from total_scattering.reduction import validateConfig
+
 
 def main(config=None):
 
     # Read in JSON if not provided to main()
     if not config:
         import argparse
+
         parser = argparse.ArgumentParser(
-            description="Absolute normalization PDF generation")
-        parser.add_argument('json', help='Input json file')
+            description="Absolute normalization PDF generation"
+        )
+        parser.add_argument("json", help="Input json file")
         options = parser.parse_args()
         print("loading config from '%s'" % options.json)
-        with open(options.json, 'r') as handle:
+        with open(options.json, "r") as handle:
             config = json.load(handle)
-    
+
     # validate the config
     validateConfig(config)
 
--- total_scattering/reduction/validator.py
+++ total_scattering/reduction/validator.py
@@ -1,5 +1,6 @@
 import logging
 
+
 def validateConfig(config: dict):
     """
     @description Validate the input config dict parsed from json
@@ -7,7 +8,7 @@ def validateConfig(config: dict):
     @param config: dict
         config dict parsed from json
     """
-    #NOTE:
+    # NOTE:
     #   All config validation should be performed prior to calling the reduction
     #   function, following the "fail early" principle.
 
@@ -25,53 +26,94 @@ def validateConfig(config: dict):
     # FullPaalmanPings   |    OK       ERR            OK             ERR       ERR
     # Mayers             |    OK       OK             ERR            OK        ERR
     # Carpenter          |    WARN     WARN           ERR            ERR       WARN
-    valid_absorption_methods =  ["None", "SampleOnly", "SampleAndContainer", "FullPaalmanPings", "Mayers", "Carpenter"]
-    valid_multiple_scattering_methods = ["None", "SampleOnly", "SampleAndContainer", "Mayers", "Carpenter"]
+    valid_absorption_methods = [
+        "None",
+        "SampleOnly",
+        "SampleAndContainer",
+        "FullPaalmanPings",
+        "Mayers",
+        "Carpenter",
+    ]
+    valid_multiple_scattering_methods = [
+        "None",
+        "SampleOnly",
+        "SampleAndContainer",
+        "Mayers",
+        "Carpenter",
+    ]
     sam_abs = config["Sample"].get("AbsorptionCorrection", None)
     sam_ms = config["Sample"].get("MultipleScatteringCorrection", None)
     if sam_abs is None:
         # missing AbsorptionCorrection section, which means no abs or ms correction
         if sam_ms is None:
-            logging.warning("No AbsorptionCorrection or MultipleScatteringCorrection found for Sample, will skip both.")
+            logging.warning(
+                "No AbsorptionCorrection or MultipleScatteringCorrection found for Sample, will skip both."
+            )
         else:
-            raise ValueError("AbsorptionCorrection section is missing but MultipleScatteringCorrection section is present")
+            raise ValueError(
+                "AbsorptionCorrection section is missing but MultipleScatteringCorrection section is present"
+            )
     else:
         sam_abs_type = sam_abs.get("Type", None)
         # quick check to make sure a known absorption correction method is used
         if str(sam_abs_type) not in valid_absorption_methods:
-            raise ValueError("AbsorptionCorrection.Type is invalid: {}".format(sam_abs_type))
+            raise ValueError(
+                "AbsorptionCorrection.Type is invalid: {}".format(sam_abs_type)
+            )
         # going through the logic map (see above)
         if sam_ms is None:
-            logging.info("The reduction will skip multiple scattering correction for sample.")
+            logging.info(
+                "The reduction will skip multiple scattering correction for sample."
+            )
         else:
             sam_ms_type = sam_ms.get("Type", None)
             # quick check to make sure a known multiple scattering correction method is used
             if str(sam_ms_type) not in valid_multiple_scattering_methods:
-                raise ValueError("MultipleScatteringCorrection.Type is invalid: {}".format(sam_ms_type))
+                raise ValueError(
+                    "MultipleScatteringCorrection.Type is invalid: {}".format(
+                        sam_ms_type
+                    )
+                )
             #
             if sam_abs_type is None:
                 if sam_ms_type is not None:
-                    raise ValueError("Any multiple scattering correction requires corresponding absorption correction")
+                    raise ValueError(
+                        "Any multiple scattering correction requires corresponding absorption correction"
+                    )
             elif sam_abs_type == "SampleOnly":
                 if sam_ms_type == "SampleAndContainer":
-                    raise ValueError("SampleAndContainer multiple scattering correction conflicts with SampleOnly absorption correction.")
+                    raise ValueError(
+                        f"ms_{sam_ms_type} is incompatible with abs_{sam_abs_type}."
+                    )
                 elif sam_ms_type == "Carpenter":
-                    logging.warning("Carpenter multiple scattering correction is only valid for Vanadium.")
+                    logging.warning(
+                        "Carpenter multiple scattering correction is only valid for Vanadium."
+                    )
                 else:
                     pass
             elif sam_abs_type in ["SampleAndContainer", "FullPaalmanPings"]:
                 if str(sam_ms_type) not in ["None", "SampleAndContainer"]:
-                    raise ValueError(f"{sam_ms_type} is incompatible with {sam_abs_type}.")
+                    raise ValueError(
+                        f"ms_{sam_ms_type} is incompatible with abs_{sam_abs_type}."
+                    )
             elif sam_abs_type == "Mayers":
                 if sam_ms_type in ["SampleAndContainer", "Carpenter"]:
-                    raise ValueError(f"{sam_ms_type} is incompatible with {sam_abs_type}.")
+                    raise ValueError(
+                        f"ms_{sam_ms_type} is incompatible with abs_{sam_abs_type}."
+                    )
             elif sam_abs_type == "Carpenter":
                 if sam_ms_type in ["SampleAndContainer", "Mayers"]:
-                    raise ValueError(f"{sam_ms_type} is incompatible with {sam_abs_type}.")
+                    raise ValueError(
+                        f"ms_{sam_ms_type} is incompatible with abs_{sam_abs_type}."
+                    )
                 else:
-                    logging.warning("Carpenter multiple scattering correction is only valid for Vanadium.")
+                    logging.warning(
+                        "Carpenter multiple scattering correction is only valid for Vanadium."
+                    )
             else:
-                logging.info(f"[Sample] will use {sam_abs_type} for absorption and {sam_ms_type} for multiple scattering")
+                logging.info(
+                    f"[Sample] will use {sam_abs_type} for absorption, {sam_ms_type} for multiple scattering"
+                )
 
     # ------------------------ #
     # Vanadium (normalization) #
@@ -87,40 +129,62 @@ def validateConfig(config: dict):
     # Carpenter          |    OK       OK         ERR       OK
     va_abs = config["Normalization"].get("AbsorptionCorrection", None)
     va_ms = config["Normalization"].get("MultipleScatteringCorrection", None)
-    valid_absorption_methods =  ["None", "SampleOnly", "Mayers", "Carpenter"]
+    valid_absorption_methods = ["None", "SampleOnly", "Mayers", "Carpenter"]
     valid_multiple_scattering_methods = ["None", "SampleOnly", "Mayers", "Carpenter"]
     if va_abs is None:
         # missing AbsorptionCorrection section, which means no abs or ms correction
         if va_ms is None:
-            logging.warning("No AbsorptionCorrection or MultipleScatteringCorrection found for Normalization, will skip both.")
+            logging.warning(
+                "Skip both absorption and multiple scattering correction."
+            )
         else:
-            raise ValueError("AbsorptionCorrection section is missing but MultipleScatteringCorrection section is present in Normalization")
+            raise ValueError(
+                "Missing absorptionCorrection but found MultipleScatteringCorrection in Normalization"
+            )
     else:
         va_abs_type = va_abs.get("Type", None)
         if str(va_abs_type) not in valid_absorption_methods:
-            raise ValueError("AbsorptionCorrection.Type is invalid: {}".format(va_abs_type))
+            raise ValueError(
+                "AbsorptionCorrection.Type is invalid: {}".format(va_abs_type)
+            )
         #
         if va_ms is None:
-            logging.info("The reduction will skip multiple scattering correction for normalization.")
+            logging.info(
+                "Will skip multiple scattering correction for normalization."
+            )
         else:
             va_ms_type = va_ms.get("Type", None)
             if str(va_ms_type) not in valid_multiple_scattering_methods:
-                raise ValueError("MultipleScatteringCorrection.Type is invalid: {}".format(va_ms_type))
+                raise ValueError(
+                    "MultipleScatteringCorrection.Type is invalid: {}".format(
+                        va_ms_type
+                    )
+                )
             #
             if va_abs_type is None:
                 if va_ms_type is None:
-                    logging.warning("skipping absorption and multiple scattering correction for normalization.")
+                    logging.warning(
+                        "skipping absorption and multiple scattering for normalization."
+                    )
                 else:
-                    raise ValueError("multiple scattering correction requires absorption correction.")
+                    raise ValueError(
+                        "multiple scattering correction requires absorption correction."
+                    )
             elif va_abs_type == "Mayers":
                 if va_ms_type == "Carpenter":
-                    raise ValueError("Carpenter multiple scattering correction is incompatible with Mayers absorption correction.")
+                    raise ValueError(
+                        "Carpenter multiple scattering correction is incompatible with Mayers absorption correction."
+                    )
                 else:
                     pass
             elif va_abs_type == "Carpenter":
                 if va_ms_type == "Mayers":
-                    raise ValueError("Mayers multiple scattering correction is incompatible with Carpenter absorption correction.")
+                    raise ValueError(
+                        "Mayers multiple scattering correction is incompatible with Carpenter absorption correction."
+                    )
                 else:
                     pass
             else:
-                logging.info(f"[Normalization] will use {va_abs_type} for absorption and {va_ms_type} for multiple scattering")
+                logging.info(
+                    f"[Normalization] will use {va_abs_type} for absorption and {va_ms_type} for multiple scattering"
+                )
