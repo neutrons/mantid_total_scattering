907102c851d388148fa383c86762dba1e44af9ea
Yuanpeng Zhang <zyroc1990@gmail.com>
2022-03-30 22:00:50 -0400

    Fix SSL verification issue

--- tests/data/remote_files.py
+++ tests/data/remote_files.py
@@ -2,6 +2,17 @@ import json
 import os
 import sys
 
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 try:
     import urllib
     fetcher = urllib.urlretrieve
